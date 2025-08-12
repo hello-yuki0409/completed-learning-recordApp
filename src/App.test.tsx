7c87e90724c17fb27f1620b34e99eb9e6240f37c
hello-yuki0409 <bobu2784@gmail.com>
2025-08-12 23:27:18 +0900

    不要なもの削除

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,4 @@
-import {
-  render,
-  screen,
-  waitForElementToBeRemoved,
-} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 jest.mock("./supabaseFunction", () => ({
